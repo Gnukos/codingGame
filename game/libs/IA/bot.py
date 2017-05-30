867e856c934731cf5bfb20919c4a4d0af20a3116
Gnukos <gnukos@gnukos.net>
2017-05-30 17:44:50 +0200

    Initial commit

---
+++ game/libs/IA/bot.py
@@ -0,0 +1,11 @@
+
+class Bot(object):
+    def __init__(self):
+        pass
+
+    def Hello(self):
+        print('hello')
+
+
+def NewBot():
+    return Bot()
---
+++ game/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from libs import IA
+
+if __name__ == '__main__':
+    agent = IA.NewBot()
+    agent.Hello()
